e62bd6c9756f32501bbbb61d8828bc93e26a3f7f
MJ Adendorff <mj.praetor@gmail.com>
2022-08-08 22:32:34 +0200

    slide1 added

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,27 @@
-import type { Component } from "solid-js";
+import type { Component, JSXElement } from "solid-js";
 import type {} from "solid-styled-jsx";
 
-const App: Component = () => {
+function One(): JSXElement {
     return (
         <>
-            <div class="main">Hello World</div>
+            <div class="main">MJ Adendorff</div>
             <style jsx dynamic>{`
                 .main {
                     background-color: black;
                     color: green;
                     height: 100vh;
-                    widht: 100%;
-                    padding: 50px;
-                    padding-left: 100px;
-                    padding-right: 100px;
+                    width: 100%;
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
                 }
             `}</style>
         </>
     );
+}
+
+const App: Component = () => {
+    return <One />;
 };
 
 export default App;

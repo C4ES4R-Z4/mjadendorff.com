67f2c1d462fc19692ffb22ad79aac7917c406d57
MJ Adendorff <mj.praetor@gmail.com>
2022-08-15 11:54:40 +0200

    Added under construction banner/notice

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,6 +1,16 @@
 import One from "./One";
 import Two from "./Two";
 import Three from "./Three";
+import { createUseStyles } from "react-jss";
+
+function UnderConstruction() {
+    const classes = styles();
+    return (
+        <div className={classes.constructionContainer}>
+            <div>UNDER CONSTRUCTION (PREVIEW)</div>
+        </div>
+    );
+}
 
 // The Page Layout
 const Home = () => {
@@ -9,8 +19,27 @@ const Home = () => {
             <One />
             <Two />
             <Three />
+            <UnderConstruction />
         </div>
     );
 };
+
+const styles = createUseStyles({
+    constructionContainer: {
+        position: "fixed",
+        minWidth: "40%",
+        textAlign: "center",
+        color: "black",
+        fontWeight: "bolder",
+        fontSize: "1.8em",
+        top: "10%",
+        left: "50%",
+        transform: "translate(-50%, -20%)",
+        padding: "20px",
+        backgroundColor: "yellow",
+        borderRadius: "10px",
+        border: "2px solid black",
+    },
+});
 
 export default Home;
